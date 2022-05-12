fa809578c18e9bab226f5d05cdddada93156a0ba
Ratul Dawar <ratuldawar11@gmail.com>
2022-05-12 15:36:22 +0530

    First commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const dotenv = require('dotenv')
+dotenv.config({ path: './config.env' });
+const cors = require('cors');
+const app = express();
+const connectToMongo = require('./config/mongoose');
+const port = process.env.PORT || 9002;
+
+app.use(cors());
+connectToMongo();
+
+app.use(express.json());
+app.use('/',require('./routes'));
+
+
+app.listen(port, () => {
+    console.log(`Fitlustarena listening at http://localhost:${port}`);
+})
