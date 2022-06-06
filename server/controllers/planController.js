229c87446f7d578b1cd8f326d87c15b7f4898652
Vinamra Mishra <vinamramishra2011@gmail.com>
2022-06-06 11:04:01 +0530

    server crash issue fixed

--- server/controllers/planController.js
+++ server/controllers/planController.js
@@ -12,16 +12,18 @@ const transporter = nodemailer.createTransport({
 });
 
 module.exports.createPlan = async (req, res, next) => {
+  var chk = 0;
   try {
     var planObject = req.body;
     const { email, name } = req.body;
     const plancheck = await Plan.find({ email: email });
     if (plancheck.length !== 0) {
+      chk = 1;
       res
         .status(200)
         .send({ message: "Oopss!! You've already applied.", status: 500 });
     }
-    
+
     var mailOptions = {
       from: "teamfitlust@yahoo.com",
       to: email,
@@ -32,32 +34,33 @@ module.exports.createPlan = async (req, res, next) => {
     console.log(error);
     res.status(500).send({ message: "Try again later.", status: 500 });
   }
-
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-      res.status(500).send({ message: "Try again later.", status: 500 });
-    } else {
+  if (chk === 0) {
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+        res.status(500).send({ message: "Try again later.", status: 500 });
+      } else {
         const plan = new Plan(planObject);
-     
-      console.log(info);
-      plan.save((err) => {
-        if (err) {
-          console.log(err);
-          res.status(500).send({ message: "Try again later.", status: 500 });
-        }else{
+
+        console.log(info);
+        plan.save((err) => {
+          if (err) {
+            console.log(err);
+            res.status(500).send({ message: "Try again later.", status: 500 });
+          } else {
             res
-            .status(200)
-            .send({
-              message: "Details recieved, we'll reach you soon",
-              status: 200,
-              done: 1,
-              detail: info,
-            });
-        }
-      });
-    }
-  });
+              .status(200)
+              .send({
+                message: "Details recieved, we'll reach you soon",
+                status: 200,
+                done: 1,
+                detail: info,
+              });
+          }
+        });
+      }
+    });
+  }
 };
 
 module.exports.getPlans = async (req, res) => {
--- server/models/Article.js
+++ server/models/Article.js
@@ -2,37 +2,37 @@ const mongoose = require('mongoose');
 
 const articleSchema = new mongoose.Schema({
     title: {
-        type:String,
-        require:true,
-        unique:true
+        type: String,
+        require: true,
+        unique: true
     },
-    image:{
-        type:String,
-        require:true,
+    image: {
+        type: String,
+        require: true,
     },
-    author:{
-        type:String,
-        required:true,
+    author: {
+        type: String,
+        required: true,
     },
-    type:{
-        type:String,
-        required:true,
+    type: {
+        type: String,
+        required: true,
     },
-    brief:{
-        type:String,
-        required:true,
+    brief: {
+        type: String,
+        required: true,
     },
-    data:[
+    data: [
         {
-            subtitle:String,
-            image:String,
-            content:String,
+            subtitle: String,
+            image: String,
+            content: String,
         }
     ],
 
-},{
-    timestamps:true,
+}, {
+    timestamps: true,
 });
-const Article =  mongoose.model('Article',articleSchema);
+const Article = mongoose.model('Article', articleSchema);
 
 module.exports = Article;
